d214586dea555c4f1822c58af54a782ed2459947
jimbene <96405180+jimbene@users.noreply.github.com>
2022-05-16 09:33:49 +0900

    Delete jmeta-view directory

--- jmeta-view/page/area.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $title = '잡인메타';
-    include_once ('./templates/header.php');
-?>  
-
--- jmeta-view/templates/footer.php
+++
@@ -1,4 +0,0 @@
-        <footer style="display:none;">footer</footer>
-    </div>
-</body>
-</html>
